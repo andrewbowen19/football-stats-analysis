e60d7893510f7aaddb89394bdf4c2fc015a3720f
andrewbowen19 <atbowen@gmail.com>
2022-04-02 10:52:33 -0400

    Initial commit, start scraper

---
+++ README.md
@@ -0,0 +1,3 @@
+Sandbox for me to test out idea for clustering on ppg vs ypg for NFL teams by season.
+
+Would like to build a visualization in plotly (jupyter-notebook) of NFL teams by PPG-YPG graphs by season (filter by season)
---
+++ src/scraper.py
@@ -0,0 +1,70 @@
+'''
+Script to scrape NFL team data for given seasons
+'''
+
+import os
+import pandas as pd
+import numpy as np
+
+
+class nflScraper(object):
+    '''
+    This class scrapes Pro-Football-Reference.com for the 3 stats most highly correlated with NFL win %:
+        - Turnover differential (turnovers created - turnovers committed)
+        - Point differential (points for - points against)
+        - One-score game win %
+        
+    class parameters:
+        season - (int or str); season desired for statistics pull (should be consistent across method calls)
+        team - str; if team != "All", only retrieves desired statistics for one team
+    '''
+
+    def __init__(self, team='All'):
+        self.team = team
+        self.candlestick_stats = None
+
+    @staticmethod
+    def format_df(df):
+        '''Remove division formatting from a PRF table df'''
+        divisions = ['NFC West', 'NFC South',
+                     'NFC North', 'NFC East',
+                     'AFC West', 'AFC South',
+                     'AFC North', 'AFC East']
+        # Dropping standings footnotes (*/+) from team names
+        df['Tm'] = df['Tm'].str.replace('*','')
+        df['Tm'] = df['Tm'].str.replace('+','')
+
+        # Setting team name to df index
+        df.set_index('Tm', inplace=True)
+        df = df.drop(divisions, axis=0)
+
+        return df
+
+    def get_standings(self, season=2021):
+        '''Scrapes PRF for NFL standings in a given season (int or str of year), '''
+
+        url = f"https://www.pro-football-reference.com/years/{season}/"
+        dfs = pd.read_html(url)
+        df = pd.concat(dfs)  # Combines AFC and NFC standings
+
+        self.standings = self.format_df(df)
+
+    def get_ypg(self, season=2021):
+        '''
+        Scraping Yards per Game (ypg) data for each team (both on offense and defense -- Opp YPG)
+        '''
+        url = f"https://www.pro-football-reference.com/years/{season}/#team_stats"
+        df = pd.read_html(url)
+        print(df)
+        
+
+    
+    def combine_data():
+        '''Combines standings and ypg stats and formats'''
+
+if __name__ == "__main__":    
+    n = nflScraper()
+    n.get_standings(2021)
+    print('-----------------------------')
+    n.get_ypg(2021)
+
